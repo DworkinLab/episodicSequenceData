ed9dcdf04de268e5fb0fdab41f78267b460f02d9
Paul Knoops <knoopsp@mcmaster.ca>
2017-01-30 12:15:33 -0500

    Need to update this data to a correct base generation note

--- R_scripts/Sync_file_working.R
+++ R_scripts/Sync_file_working.R
@@ -4,7 +4,9 @@
 #Data_subset <- read.table("~/Bioinformatics/Sequence_analysis_2016/episodic_data_2R.sync")
 # == 3.6 Gb
 
-Data_subset <- read.table("~/Bioinformatics/Sequence_analysis_2016/Data/episodic_data_Ian_subset.sync")
+Data_subset <- read.table("episodic_data_Ian_subset.sync")
 # == 194 KB
-# --> old data (incorrect base generation?)
+#old data (incorrect base generation?)
+# Same method into Git_journal to get a random region of one chromosome.....
+
 head(Data_subset)
