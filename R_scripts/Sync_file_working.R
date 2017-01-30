c875bb0266496f8f2189709d6a5680a29b746662
Paul Knoops <knoopsp@mcmaster.ca>
2017-01-30 12:10:47 -0500

    commit

--- R_scripts/Sync_file_working.R
+++ R_scripts/Sync_file_working.R
@@ -1,2 +1,10 @@
-Data_subset <- read.table("R Data/episodic_data_2R.sync")
-Data_subset
+
+
+#To Large
+#Data_subset <- read.table("~/Bioinformatics/Sequence_analysis_2016/episodic_data_2R.sync")
+# == 3.6 Gb
+
+Data_subset <- read.table("~/Bioinformatics/Sequence_analysis_2016/Data/episodic_data_Ian_subset.sync")
+# == 194 KB
+# --> old data (incorrect base generation?)
+head(Data_subset)
