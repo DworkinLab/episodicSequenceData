86b2c6bd1c272f77a03fef03c452e60474db3293
Paul Knoops <knoopsp@mcmaster.ca>
2017-01-31 11:32:19 -0500

    plot fail

--- R_scripts/Sync_file_working.R
+++ R_scripts/Sync_file_working.R
@@ -12,5 +12,4 @@ Data_subset <- read.table("episodic_data_Ian_subset.sync")
 head(Data_subset)
 colnames(Data_subset) <- c("Chromosome", "Position", "ref", "115ConR1", "115ConR2", "115SelR1", "115SelR2", "38ConR1", "38ConR2", "38SelR1", "38SelR2", "77ConR1", "77ConR2", "77SelR1", "77SelR2", "MDG2", "MGD")
 head(Data_subset)
-
 #Use tidyr --> make long?
