c55d8313a66b256016ba77ffa7846d02584cbaaa
Paul Knoops <knoopsp@mcmaster.ca>
2017-02-01 11:11:57 -0500

    notes on files

--- R_scripts/Sync_file_working.R
+++ R_scripts/Sync_file_working.R
@@ -8,7 +8,10 @@ library(dplyr)
 
 #Random Region near middle of chromosome == 10000 bp
 Data_subset <- read.table("episodic_data_2R_subset.sync")
-# == 1.8 MB
+# == 1.8 MB -- Note; have a smaller file (184kb)
+
+
+##Format of populations ex. 27:0:0:30:0:0 == A-count:T-count:C-count:G-count:N-count:deletion-count
 
 head(Data_subset)
 colnames(Data_subset) <- c("Chromosome", "Position", "ref", "ConR1_115", "ConR2_115", "SelR1_115", "SelR2_115", "ConR1_38", "ConR2_38", "SelR1_38", "SelR2_38", "ConR1_77", "ConR2_77", "SelR1_77", "SelR2_77", "Base")
@@ -16,6 +19,6 @@ head(Data_subset)
 
 #Use tidyr --> make long?
 Episodic_long <- gather(Data_subset, Population, Allele_Freq , ConR1_115:Base, factor_key=TRUE)
-Episodic_long
+head(Episodic_long)
 
-print(with(Episodic_long) plot)
+
