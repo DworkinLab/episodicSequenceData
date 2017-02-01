3af708f42384e47427e4ed78c6f397e7853da30e
Paul Knoops <knoopsp@mcmaster.ca>
2017-02-01 15:33:59 -0500

    as.numeric and many plots

--- R_scripts/Sync_file_working.R
+++ R_scripts/Sync_file_working.R
@@ -21,7 +21,6 @@ Data_subset <- read.table("episodic_data_2R_1000_subset.sync")
 colnames(Data_subset) <- c("Chromosome", "Position", "ref", "ConR1_115", "ConR2_115", "SelR1_115", "SelR2_115", "ConR1_38", "ConR2_38", "SelR1_38", "SelR2_38", "ConR1_77", "ConR2_77", "SelR1_77", "SelR2_77", "Gen0")
 
 print(head(Data_subset))
-
 # Use tidyr --> make long
 Episodic_long <- gather(Data_subset, Population, Allele_Freq , ConR1_115:Gen0, factor_key=TRUE)
 
@@ -38,22 +37,24 @@ head(Episodic_seperate)
 
 #Make Longer??
 longer_Episodic <- gather(Episodic_seperate, Base, Count, A:del, factor_key = TRUE)
-
-head(longer_Episodic)
+longer_Episodic$Count <- as.numeric(longer_Episodic$Count)
+longer_Episodic$Position <- as.numeric(longer_Episodic$Position)
 
 #Remove all those with count == 0
 Epi_rem <- subset(longer_Episodic, Count>0)
 head(Epi_rem)
 
-summary(Epi_rem)
-
 #Plot for base generation: first subset
-
-#note= easier to see (and likely fix) with smaller (1000bp) start
-
 Epi_Base <- subset(Epi_rem, Population =="Gen0")
+Epi_Base <- arrange(Epi_Base, Position)
+head(Epi_Base)
+
 
 p1 <- ggplot(data = Epi_Base, 
              aes(x = Position, y=Count, colour=Base))
-p2 <- (p1 + geom_point(size = 2, alpha=0.5))
-print(p2)
+p2 <- ggplot(data = Epi_Base, 
+                   aes(x = Position, y=ref, colour=Base))
+p3 <- (p1 + geom_point(size = 2, alpha=0.5))
+p4 <- (p2 + geom_point(alpha=0.5))
+print(p3)
+print(p4)
