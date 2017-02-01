ce96d84d340eb0cc8334250e5636eaa893d58407
Paul Knoops <knoopsp@mcmaster.ca>
2017-02-01 10:11:38 -0500

    Delete old Ian_subset and new working with .sync

--- R_scripts/Sync_file_working.R
+++ R_scripts/Sync_file_working.R
@@ -4,12 +4,15 @@
 #Data_subset <- read.table("~/Bioinformatics/Sequence_analysis_2016/episodic_data_2R.sync")
 # == 3.6 Gb
 
-Data_subset <- read.table("episodic_data_Ian_subset.sync")
-# == 194 KB
-#old data (incorrect base generation?)
-# Same method into Git_journal to get a random region of one chromosome.....
+#Random Region near middle of chromosome == 10000 bp
+Data_subset <- read.table("episodic_data_2R_subset.sync")
+# == 1.8 MB
+
 
 head(Data_subset)
-colnames(Data_subset) <- c("Chromosome", "Position", "ref", "115ConR1", "115ConR2", "115SelR1", "115SelR2", "38ConR1", "38ConR2", "38SelR1", "38SelR2", "77ConR1", "77ConR2", "77SelR1", "77SelR2", "MDG2", "MGD")
+colnames(Data_subset) <- c("Chromosome", "Position", "ref", "115ConR1", "115ConR2", "115SelR1", "115SelR2", "38ConR1", "38ConR2", "38SelR1", "38SelR2", "77ConR1", "77ConR2", "77SelR1", "77SelR2", "Base")
 head(Data_subset)
+
+
+
 #Use tidyr --> make long?
