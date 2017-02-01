85829e2a97534bd8ae5ee81df0b57aa6197ece7e
Paul Knoops <knoopsp@mcmaster.ca>
2017-02-01 10:38:30 -0500

    checking data

--- R_scripts/Sync_file_working.R
+++ R_scripts/Sync_file_working.R
@@ -10,11 +10,10 @@ library(dplyr)
 Data_subset <- read.table("episodic_data_2R_subset.sync")
 # == 1.8 MB
 
-
 head(Data_subset)
 colnames(Data_subset) <- c("Chromosome", "Position", "ref", "ConR1_115", "ConR2_115", "SelR1_115", "SelR2_115", "ConR1_38", "ConR2_38", "SelR1_38", "SelR2_38", "ConR1_77", "ConR2_77", "SelR1_77", "SelR2_77", "Base")
 head(Data_subset)
 
 #Use tidyr --> make long?
 Episodic_long <- gather(Data_subset, Population, Allele_Freq , ConR1_115:Base, factor_key=TRUE)
-Episodic_long$Population
+Episodic_long
