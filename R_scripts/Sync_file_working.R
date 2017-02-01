c2218afce719a4ca295ed75b58d1a6252c21570f
Paul Knoops <knoopsp@mcmaster.ca>
2017-02-01 11:14:56 -0500

    notes to make for 708

--- R_scripts/Sync_file_working.R
+++ R_scripts/Sync_file_working.R
@@ -2,23 +2,26 @@ library(tidyr)
 library(ggplot2)
 library(dplyr)
 
-#To Large
+
+#Data from only the Right arm of the 2nd Chromosome
 #Data_subset <- read.table("~/Bioinformatics/Sequence_analysis_2016/episodic_data_2R.sync")
-# == 3.6 Gb
+# File size == 3.6 Gb --> to large, use a subset
 
 #Random Region near middle of chromosome == 10000 bp
-Data_subset <- read.table("episodic_data_2R_subset.sync")
 # == 1.8 MB -- Note; have a smaller file (184kb)
 
+Data_subset <- read.table("episodic_data_2R_subset.sync")
 
-##Format of populations ex. 27:0:0:30:0:0 == A-count:T-count:C-count:G-count:N-count:deletion-count
+#Format of populations ex. 27:0:0:30:0:0 == A-count:T-count:C-count:G-count:N-count:deletion-count
 
-head(Data_subset)
+#adjust colnames
+
 colnames(Data_subset) <- c("Chromosome", "Position", "ref", "ConR1_115", "ConR2_115", "SelR1_115", "SelR2_115", "ConR1_38", "ConR2_38", "SelR1_38", "SelR2_38", "ConR1_77", "ConR2_77", "SelR1_77", "SelR2_77", "Base")
-head(Data_subset)
+
+print(head(Data_subset))
 
 #Use tidyr --> make long?
 Episodic_long <- gather(Data_subset, Population, Allele_Freq , ConR1_115:Base, factor_key=TRUE)
-head(Episodic_long)
+print(head(Episodic_long))
 
 
