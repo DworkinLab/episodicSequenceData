4a4d0ea1b573eb887174630197d38284ccdf5ae7
Paul Knoops <knoopsp@mcmaster.ca>
2017-02-02 11:50:59 -0500

    new arrangment of table

--- R_scripts/Sync_file_working.R
+++ R_scripts/Sync_file_working.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(dplyr)
 
 
+
 #Data from only the Right arm of the 2nd Chromosome
 #Data_subset <- read.table("~/Bioinformatics/Sequence_analysis_2016/episodic_data_2R.sync")
 # File size == 3.6 Gb --> to large, use a subset
@@ -18,11 +19,11 @@ Data_subset <- read.table("episodic_data_2R_1000_subset.sync")
 
 #adjust colnames
 
-colnames(Data_subset) <- c("Chromosome", "Position", "ref", "ConR1_115", "ConR2_115", "SelR1_115", "SelR2_115", "ConR1_38", "ConR2_38", "SelR1_38", "SelR2_38", "ConR1_77", "ConR2_77", "SelR1_77", "SelR2_77", "Gen0")
+colnames(Data_subset) <- c("Chromosome", "Position", "ref", "ConR1_115", "ConR2_115", "SelR1_115", "SelR2_115", "ConR1_38", "ConR2_38", "SelR1_38", "SelR2_38", "ConR1_77", "ConR2_77", "SelR1_77", "SelR2_77", "BaseR1_0")
 
 print(head(Data_subset))
 # Use tidyr --> make long
-Episodic_long <- gather(Data_subset, Population, Allele_Freq , ConR1_115:Gen0, factor_key=TRUE)
+Episodic_long <- gather(Data_subset, Population, Allele_Freq , ConR1_115:BaseR1_0, factor_key=TRUE)
 
 print(head(Episodic_long))
 #Seperate the allele counts into independent columns for each base
@@ -31,27 +32,16 @@ Episodic_seperate <- Episodic_long %>%
 
 print(head(Episodic_seperate))
 
-#Make Longer
+#Make Longer -- may not be needed from here onward until after plots
 longer_Episodic <- gather(Episodic_seperate, Base, Count, A:del, factor_key = TRUE)
-
-print(head(longer_Episodic))
-
 #numeric counts and arrange by position
 longer_Episodic$Count <- as.numeric(longer_Episodic$Count)
 longer_Episodic <- arrange(longer_Episodic, Position)
-
-
 #Remove all those with count == 0
 Epi_rem <- subset(longer_Episodic, Count>0)
-print(head(Epi_rem))
-
 #Plot for base generation: first subset
-
-Epi_Base <- subset(Epi_rem, Population =="Gen0")
+Epi_Base <- subset(Epi_rem, Population =="BaseR1_0")
 Epi_Base <- arrange(Epi_Base, Position)
-head(Epi_Base)
-
-
 p1 <- ggplot(data = Epi_Base, 
              aes(x = Position, y=Count, colour=Base))
 p2 <- ggplot(data = Epi_Base, 
@@ -60,3 +50,25 @@ p3 <- (p1 + geom_point(size = 2, alpha=0.5))
 p4 <- (p2 + geom_point(alpha=0.5))
 print(p3)
 print(p4)
+
+# Keep from here
+
+#Want to rearrange data: start with Episodic_seperate
+print(head(Episodic_seperate))
+
+#Split Population into Treatment, Rep, and Generation
+Episoidic_split <- Episodic_seperate %>%
+  separate(Population, c("Treatment", "Generation"), "_")
+
+Episodic_split <- Episodic_split %>%
+  separate(Treatment, c("Treatment", "Replicate"), "R")
+
+
+#as.numeric to multiple columns:
+cols.num <- c("Replicate","Generation", "A", "T", "C", "G", "N", "del")
+Episodic_split[cols.num] <- sapply(Episodic_split[cols.num],as.numeric)
+
+#Drop Chromosome
+Epi_Pop_split <- subset(Episodic_split, select = -c(Chromosome) )
+
+print(head(Episodic_split))
