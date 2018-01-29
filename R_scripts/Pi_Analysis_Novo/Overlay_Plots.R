d31e80e76809824f038dc4b77eb2ffcc8719d16d
Paul Knoops <knoopsp@mcmaster.ca>
2018-01-29 12:15:12 -0500

    overlay plots

--- R_scripts/Pi_Analysis_Novo/Overlay_Plots.R
+++ R_scripts/Pi_Analysis_Novo/Overlay_Plots.R
@@ -44,7 +44,8 @@ for (file in MyPi){
   DattFull <- rbind(DattX, Datt2L, Datt2R, Datt3L, Datt3R, Datt4)
   
   #Pi as numeric
-  DattFull$Pi=as.numeric(levels(DattFull$Pi))[DattFull$Pi]
+  DattFull$Pi=as.numeric(lev
+                         els(DattFull$Pi))[DattFull$Pi]
   DattFull$Seq <- x2
   assign(paste("Datt", x2, sep="_"),DattFull)
 }
