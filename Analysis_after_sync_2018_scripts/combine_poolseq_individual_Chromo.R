c047bd44358cdff5f0287debd9f9061c646709bd
PaulKnoops <PaulKnoops@users.noreply.github.com>
2018-04-06 14:57:42 -0400

    Update combine_poolseq_individual_Chromo.R

--- Analysis_after_sync_2018_scripts/combine_poolseq_individual_Chromo.R
+++ Analysis_after_sync_2018_scripts/combine_poolseq_individual_Chromo.R
@@ -1,12 +1,15 @@
-#setwd('/home/paul/episodicData/novoalign/novo_mpileup/splitsync_dir/novo_episodic_2L_Con_Split')
+# set directory holding all .csv files to combine
 setwd('/home/paul/episodicData/novoalign/novo_mpileup/splitsync_dir/novo_episodic_2L_Sel_Split')
 
+#list Csvs
 mycsvs <- list.files(pattern='.csv')
 X <- NULL
 for (file in mycsvs){
   X2 <- read.csv(file, h=T)
   X <- rbind(X, X2)
 }
+# change based on chromo!
 X$chr <- '2L'
-#write.csv(X, file='/home/paul/episodicData/novoalign/novo_mpileup/novo_episodic_2L_Con.csv', row.names = FALSE)
+
+#write the CSV file !!! EASY PEASY
 write.csv(X, file='/home/paul/episodicData/novoalign/novo_mpileup/novo_episodic_2L_Sel.csv', row.names = FALSE)
